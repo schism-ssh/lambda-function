c5ad02bdda6b36f48b04aa20658a98fdfd855c7d
Anthony Gargiulo <anthony@agargiulo.com>
2020-04-21 01:56:14 -0700

    Initial work to save signed certificates to S3

--- internal/cloud/aws_test.go
+++ internal/cloud/aws_test.go
@@ -3,12 +3,16 @@ package cloud
 import (
 	"errors"
 	"reflect"
-	"src.doom.fm/schism/lambda-function/internal/crypto"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/aws/aws-sdk-go/service/ssm/ssmiface"
+
+	"src.doom.fm/schism/commonLib/protocol"
+	"src.doom.fm/schism/lambda-function/internal/crypto"
 )
 
 type mockSSMClient struct {
@@ -31,6 +35,14 @@ func (m *mockSSMClient) GetParameter(input *ssm.GetParameterInput) (*ssm.GetPara
 	} else {
 		return nil, errors.New("InvalidKeyId")
 	}
+}
+
+type mockS3Client struct {
+	s3iface.S3API
+}
+
+func (m *mockS3Client) PutObject(input *s3.PutObjectInput) (*s3.PutObjectOutput, error) {
+	return &s3.PutObjectOutput{}, nil
 }
 
 func TestSaveCAToSSM(t *testing.T) {
@@ -106,6 +118,59 @@ func TestLoadCAFromSSM(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("LoadCAFromSSM() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSaveCertToS3(t *testing.T) {
+	type args struct {
+		s3Svc    s3iface.S3API
+		s3Bucket string
+		s3Object *protocol.SignedCertificateS3Object
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "save host cert",
+			args: args{
+				s3Svc: &mockS3Client{}, s3Bucket: "schism-test",
+				s3Object: &protocol.SignedCertificateS3Object{
+					CertificateType: "host",
+					Identity:        "test.schism.example.com",
+					Principals:      []string{"test.schism.example.com"},
+				},
+			},
+			want:    "hosts/73f386e91cac74186f60ba0aca0a410c234b3cfafb68f20541e4c5a828a1491b.json",
+			wantErr: false,
+		},
+		{
+			name: "save user cert",
+			args: args{
+				s3Svc: &mockS3Client{}, s3Bucket: "schism-test",
+				s3Object: &protocol.SignedCertificateS3Object{
+					CertificateType: "user",
+					Identity:        "user@test.schism.example.com",
+					Principals:      []string{"user1", "app_user"},
+				},
+			},
+			want:    "users/1d2206f7294dedac0c991bbf3656db48a7e93cc913c7e467c4c9d2d6149ab83c.json",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := SaveCertToS3(tt.args.s3Svc, tt.args.s3Bucket, tt.args.s3Object)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SaveCertToS3() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("SaveCertToS3() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
--- internal/cloud/support.go
+++ internal/cloud/support.go
@@ -1,10 +1,15 @@
 package cloud
 
 import (
+	"crypto/sha256"
+	"fmt"
 	"os"
+	"sort"
+	"strings"
 )
 
 const CaParamPrefixEnvVar = "SCHISM_CA_PARAM_PREFIX"
+const CertsS3BucketEnvVar  = "SCHISM_CERTS_S3_BUCKET"
 
 func CaParamPrefix() string {
 	caParamPrefix, keyFound := os.LookupEnv(CaParamPrefixEnvVar)
@@ -12,4 +17,19 @@ func CaParamPrefix() string {
 		caParamPrefix = "schism-"
 	}
 	return caParamPrefix
+}
+
+func CertsS3Bucket() string {
+	certsS3Bucket, keyFound := os.LookupEnv(CertsS3BucketEnvVar)
+	if !keyFound || len(certsS3Bucket) == 0 {
+		certsS3Bucket = "schism-signed-certificates"
+	}
+	return certsS3Bucket
+}
+
+func LookupKey(ident string, princs []string) string {
+	sort.Strings(princs)
+	lookupList := append([]string{ident}, princs...)
+	lookupString := strings.Join(lookupList, ",")
+	return fmt.Sprintf("%x", sha256.Sum256([]byte(lookupString)))
 }
